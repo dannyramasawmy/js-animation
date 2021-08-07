e9ef17473e66b72dd5e0a692b6dd967099c1bf64
Danny Ramasawmy <dannyramasawmy@gmail.com>
2021-08-07 15:58:16 +0100

    First commit - draw a rectangle on screen.

---
+++ drawing_canvas.js
@@ -0,0 +1,10 @@
+
+// Set canvas H and W
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+// drawing
+var c = canvas.getContext('2d')
+c.fillRect(100, 100, 100, 100 );
