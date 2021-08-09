a13488c2dfc8304e60b7e3b51f4ebae1bfedfec6
Danny Ramasawmy <dannyramasawmy@gmail.com>
2021-08-09 16:54:32 +0100

    Added snow animation.

--- snow.js
+++ snow.js
@@ -6,41 +6,16 @@ canvas.height = window.innerHeight;
 // drawing
 var c = canvas.getContext('2d')
 
-var mouse = {
-    x : undefined,
-    y : undefined,
-    d : false,
-    vmult : 1
-}
-
-// interaction
-window.addEventListener('mousemove',
-    function(event) {
-        mouse.x = event.x
-        mouse.y = event.y
-    })
-window.addEventListener('mousedown',
-    function(event) {
-        mouse.d = true
-        mouse.vmult = -1.5
-    })
-window.addEventListener('mouseup',
-    function(event) {
-        mouse.d = false
-        mouse.vmult = 1
-    })
-
     
-
 // Color info:
 // Carmen Russmann
 // https://color.adobe.com/search?q=warm&t=term
 var colorArray = [
-    '#291B19',
-    '#794034',
-    '#B96D47',
-    '#D89848',
-    '#6E8B76',
+    '#CEE4F2',
+    '#9CC1D9',
+    '#75A3BF',
+    '#46718C',
+    '#1A1E26',
 ];
 
 window.addEventListener('resize', function(event) {
@@ -56,7 +31,7 @@ function Circle(x, y, vx, vy, radius, color, tdecay) {
     this.vx = vx;
     this.vy = vy;
     this.radius = radius;
-    this.maxRadius = 60;
+    this.maxRadius = 50;
     this.minRadius = radius
     this.color = color;
     self.tdecay = tdecay
@@ -73,29 +48,13 @@ function Circle(x, y, vx, vy, radius, color, tdecay) {
         if (this.x > innerWidth-this.radius || this.x < this.radius ) {
             this.vx = -this.vx
         }
-        if (this.y > innerHeight-this.radius || this.y < this.radius ) {
-            this.vy = -this.vy
+        if (this.y > innerHeight ) {
+            this.y = this.radius
         }
    
         // update velocity
-        this.x += this.vx * mouse.vmult;
-        this.y += this.vy * mouse.vmult;
-
-        // if near mouse
-        var radiusOfInfluence = 75; 
-        if ((Math.abs(mouse.x - this.x) < radiusOfInfluence) && (Math.abs(mouse.y - this.y) < radiusOfInfluence) ){
-            if (this.radius < this.maxRadius) {
-                this.radius += 0.5
-            // user clicks
-            var fac = 1.5
-            this.x +=  (mouse.x - this.x)/Math.abs(mouse.x - this.x) * fac
-            this.y +=  (mouse.y - this.y)/Math.abs(mouse.y - this.y) * fac
-            }
-        } else {
-            if (this.radius > this.minRadius) {
-                this.radius -= self.tdecay
-            }
-        }
+        this.x += this.vx ;
+        this.y += this.vy + 1;
         
         this.draw();
     };
@@ -111,8 +70,8 @@ function init() {
         var radius =  Math.random() * 5 + 1;
         var x = Math.random() * (innerWidth - 2*radius) + radius;
         var y = Math.random() * (innerHeight - 2*radius) + radius;;
-        var vx =  Math.round((Math.random() - 0.5) * 5) ;
-        var vy = Math.round((Math.random() - 0.5) * 5) ;
+        var vx =  Math.round((Math.random() - 0.5) * 2) ;
+        var vy = Math.round((Math.random() + 0.1) * 2) ;
         var tdecay = Math.random() * 0.4 
         color = colorArray[Math.floor(Math.random() * colorArray.length)]
         circle.push(new Circle(x, y, vx, vy, radius, color, tdecay))
@@ -120,12 +79,21 @@ function init() {
 
 }
 
+// background color gradient
+gradient = c.createLinearGradient(0, 0, 0, innerHeight)
+gradient.addColorStop(0, 'black');
+gradient.addColorStop(1, 'gray');
+
+
 // call a recursive like function to draw each frame
 function animate() {
     requestAnimationFrame(animate)
 
     // reset the canvas
-    c.clearRect(0, 0, innerWidth, innerHeight);
+    // c.clearRect(0, 0, innerWidth, innerHeight);
+    // Set the fill style and draw a rectangle
+    c.fillStyle = gradient;
+    c.fillRect(0, 0, innerWidth, innerHeight);
 
     // update all circles
     for (var i = 0; i < circle.length; i++) {
