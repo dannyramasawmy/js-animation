052646962d36109e2060d75909fd87a2daad56b6
Danny Ramasawmy <dannyramasawmy@gmail.com>
2021-08-11 20:48:09 +0100

    Refactoring sun objects.

--- scenes/sun.js
+++ scenes/sun.js
@@ -5,7 +5,7 @@ canvas.height = window.innerHeight;
 // drawing
 var c = canvas.getContext('2d')
 
-    
+
 // Color info:
 // Carmen Russmann
 // https://color.adobe.com/search?q=warm&t=term
@@ -24,14 +24,14 @@ var colorArray = [
 // #DB5817
 
 var circle = [];
-window.addEventListener('resize', function(event) {
+window.addEventListener('resize', function (event) {
     //console.log(event)
     circle = []
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     init(100);
     background()
-    
+
 });
 
 function Circle(x, y, vx, vy, radius, color, tdecay) {
@@ -47,18 +47,10 @@ function Circle(x, y, vx, vy, radius, color, tdecay) {
     this.redness = 150
     this.tf = 0.004
 
-    this.draw = function() {
-
-        // c.beginPath();
-        // c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        // c.fillStyle = `rgba(255, ${self.color}, 0, ${this.tdecay})`;
-        // c.fill();
-
-
-
+    this.draw = function () {
         c.beginPath();
         c.moveTo(this.x, this.y)
-        c.lineTo(this.x+this.vx*innerWidth*0.05 , this.y + this.vy*innerWidth*0.05)
+        c.lineTo(this.x + this.vx * innerWidth * 0.05, this.y + this.vy * innerWidth * 0.05)
         // c.strokeStyle  = `rgba(255, ${255}, 0, ${0.1})`;
         c.strokeStyle = `rgba(255, ${self.color}, 0, ${this.tdecay})`;
         c.lineWidth = 5;
@@ -68,107 +60,93 @@ function Circle(x, y, vx, vy, radius, color, tdecay) {
 
     };
 
-    this.update = function() {
-        // wall collision
-        // if (this.x > innerWidth-this.radius || this.x < this.radius ) {
-        //     this.vx = -this.vx
-        // }
-        // if (this.y > innerHeight ) {
-        //     this.y = this.radius
-        // }
-   
+    this.update = function () {
         // update velocity
-        this.x += this.vx ;
-        this.y += this.vy ;
+        this.x += this.vx;
+        this.y += this.vy;
         this.tdecay -= this.tf;
-        this.redness = Math.round(this.redness + 0.5) 
-        
+        this.redness = Math.round(this.redness + 0.5)
+
         this.draw();
     };
 };
 
 
 
-
-var mymounts = []
-
-var xs = []
-var height_ =[]
-var width_ = []
-var gray_ = []
-var muns = 100
-for (var i = 0; i < muns; i++) {
-    xs.push( (Math.random() ) * innerWidth)
-    height_.push( (Math.random()*150 + 50))
-    width_.push( (Math.random()*150 + 50))
-    gray_.push( Math.round(Math.random()*100))
-    mymounts.push(new drawMountain(xs[i], height_[i], width_[i], gray_[i]))
-}
-
-
-
-function drawMountain(x, h, w , cols) {
+function drawMountain(x, h, w, cols) {
     this.x = x
     this.height = h
     this.width = w
     this.gray = cols
-    this.y = innerHeight*0.6
+    this.y = innerHeight * 0.6
 
 
-    this.draw = function() {
+    this.draw_ = function(reflection) {
+        // positive mountains
         c.beginPath();
-        c.moveTo( this.x , this.y - this.height)
-        c.lineTo( this.x + this.width  , this.y )
-        c.lineTo( this.x -  this.width,  this.y )
-        c.fillStyle = `rgba(${this.gray}, ${this.gray}, ${this.gray}, ${1})`
+        if (reflection) 
+        {   // mountains
+            c.fillStyle = `rgba(${this.gray}, ${this.gray}, ${this.gray}, ${1})`
+            c.moveTo(this.x, this.y - this.height)
+        }
+        else 
+        {   // reflections
+            c.fillStyle = `rgba(${this.gray}, ${this.gray}, ${this.gray + 50}, ${0.7} )`
+            c.moveTo(this.x, this.y + this.height)
+        }
+        c.lineTo(this.x + this.width, this.y)
+        c.lineTo(this.x - this.width, this.y)
         c.fill();
+    }
+
+    this.draw = function () {
+        // draw mountain
+        this.draw_(false)
+        // draw reflection
+        this.draw_(true)
     };
 };
 
 
-
-
-function drawMounts() {
-    for (var i =0; i < muns; i++) {
-        // console.log(xs[i])
-        // console.log(height_[i])
-        // console.log(width_[i])
-        // console.log(gray_[i])
-
-
-
-
+function drawSun() {
+    this.draw = function () {
         c.beginPath();
-        c.moveTo( xs[i] ,innerHeight*0.6 - height_[i])
-        c.lineTo( xs[i] + width_[i]  , innerHeight*0.6 )
-        c.lineTo( xs[i] - width_[i], innerHeight*0.6 )
-        c.fillStyle = `rgba(${gray_[i]}, ${gray_[i]}, ${gray_[i]}, ${1})`
+        c.arc(0, 0, innerWidth * 0.1, 0, Math.PI * 2, false);
+        c.fillStyle = grd;
         c.fill();
-
-        c.beginPath();
-        c.moveTo( xs[i] ,innerHeight*0.6 + height_[i])
-        c.lineTo( xs[i] + width_[i]  , innerHeight*0.6 )
-        c.lineTo( xs[i] - width_[i], innerHeight*0.6 )
-        c.lineWidth = 5;
-        c.fillStyle = `rgba(${gray_[i]}, ${gray_[i]}, ${gray_[i]+50}, ${0.7} )`
-        c.fill();
-
-        }
+    }
 }
 
 
 
-// generate circles
+var mymounts = []
 
+var xs = undefined
+var height_ = undefined
+var width_ = undefined
+var gray_ = undefined
+var muns = 100
+for (var i = 0; i < muns; i++) {
+    xs = (Math.random() * innerWidth)
+    height_ = ((Math.random() * 150 + 50))
+    width_ = ((Math.random() * 150 + 50))
+    gray_ = (Math.round(Math.random() * 100))
+    mymounts.push(new drawMountain(xs, height_, width_, gray_))
+}
+
+
+
+
+// generate circles
 var numCircles = 5;
 function init(numCircles) {
     for (var i = 0; i < numCircles; i++) {
-        var radius =  Math.random() * 5 + 1;
+        var radius = Math.random() * 5 + 1;
         // var x = Math.random() * (innerWidth - 2*radius) + radius;
         // var y = Math.random() * (innerHeight - 2*radius) + radius;;
-        var vx =  Math.random() ;
-        var vy = Math.random() ;
-        var tdecay = Math.random() * 0.4 
+        var vx = Math.random();
+        var vy = Math.random();
+        var tdecay = Math.random() * 0.4
         color = colorArray[Math.floor(Math.random() * colorArray.length)]
         circle.push(new Circle(radius, radius, vx, vy, radius, color, tdecay))
     }
@@ -187,13 +165,13 @@ gradient.addColorStop(0.8, '#04B2D9');
 gradient.addColorStop(1, '#0669BF');
 
 
-grd = c.createLinearGradient(0, 0, innerWidth*0.1, innerHeight*0.1)
+grd = c.createLinearGradient(0, 0, innerWidth * 0.1, innerHeight * 0.1)
 grd.addColorStop(0, `rgb(255, 255, 0)`);
 grd.addColorStop(1, 'rgb(255, 100, 0)');
 
 function background() {
-c.fillStyle = gradient;
-c.fillRect(0, 0, innerWidth, innerHeight);
+    c.fillStyle = gradient;
+    c.fillRect(0, 0, innerWidth, innerHeight);
 }
 count = 100
 background()
@@ -205,9 +183,6 @@ background()
 function animate() {
     requestAnimationFrame(animate)
 
-    // reset the canvas
-    // c.clearRect(0, 0, innerWidth, innerHeight);
-    // Set the fill style and draw a rectangle
     background()
 
     // update all circles
@@ -219,20 +194,19 @@ function animate() {
         }
     }
 
-
-    c.beginPath();
-    c.arc(0, 0, innerWidth*0.1, 0, Math.PI * 2, false);
-    c.fillStyle = grd;
-    c.fill();
+    // draw the sun
+    theSun.draw();
 
     // drawMounts()
-    for (var i = 0;  i< muns; i ++) {
-    mymounts[i].draw()
+    for (var i = 0; i < muns; i++) {
+        mymounts[i].draw()
     }
 
 }
 
-for (var i = 0; i < 100 ; i ++ ) {
+var theSun = new drawSun();
+
+for (var i = 0; i < 100; i++) {
     init(1);
 }
 animate();
